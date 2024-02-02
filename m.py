4102a7a975b8b24883082adf7a7218edfe5d1321
cvaz1306 <christophervaz160@gmail.com>
2024-02-01 16:19:49 -0800

    Added multithreadedness

--- m.py
+++ m.py
@@ -1,7 +1,9 @@
 import sys_comms_refresh
+
+import multiprocessing
 print("Service started")
 try:
-    sys_comms_refresh.reset_interface(.1)
+    p = multiprocessing.Process(target=sys_comms_refresh.reset_interface, args=(0.1,))
 except KeyboardInterrupt:
     print("Service ended with keyboard.")
 except:
