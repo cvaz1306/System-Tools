175ab39f21a8548438d4ffad571feaa28a033276
BuildTools <unconfigured@null.spigotmc.org>
2024-02-01 17:44:50 -0800

    Added additional error handling

--- m.py
+++ m.py
@@ -1,10 +1,14 @@
 import sys_comms_refresh
 
 import multiprocessing
-print("Service started")
-try:
-    p = multiprocessing.Process(target=sys_comms_refresh.reset_interface, args=(0.1,))
-except KeyboardInterrupt:
-    print("Service ended with keyboard.")
-except:
-    print("Error")
+if __name__=='__main__':
+    
+    print("Service started")
+    try:
+        p = multiprocessing.Process(target=sys_comms_refresh.reset_interface, args=(0.1,))
+        p.start()
+        p.join()
+    except KeyboardInterrupt:
+        print("Service ended with keyboard.")
+    except Exception as e:
+        print(f"Error: {e}")
--- sys_comms_refresh.py
+++ sys_comms_refresh.py
@@ -1,6 +1,9 @@
 import ctypes
 from time import sleep
 def reset_interface(interval):
-    while True:
-        ctypes.windll.user32.SendMessageW(0xFFFF, 0x0214, 0, 0)
-        sleep(interval)
+    try:
+        while True:
+            ctypes.windll.user32.SendMessageW(0xFFFF, 0x0214, 0, 0)
+            sleep(interval)
+    except:
+        print("Service sys_comms_refresh interrupted.")
